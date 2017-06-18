18d0585ff2c3243e53ed0f0b860b06e9a617c811
olegfineev <fineevoleg@gmail.com>
2017-06-18 19:16:23 +0300

    about section

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,26 @@
 function initPage(){
 
-	// your functions go here
+	// steickyHeader function
+  var stickyHeader;
+  window.wh = $(window).height() - 80;
+  $(window).bind("resize", function() {
+    return window.wh = $(window).height() - 80;
+  });
+
+  stickyHeader = function() {
+    var $elem, current_position, offset;
+    $elem = $('#header');
+    current_position = $(window).scrollTop();
+    offset = 40;
+    if (current_position > offset && $elem.hasClass('top')) {
+      return $elem.removeClass('top').addClass('sticky');
+    } else if (current_position <= offset && $elem.hasClass('sticky')) {
+      return $elem.removeClass('sticky').addClass('top');
+    }
+  };
+
+  $(window).on('scroll', function() {
+    return stickyHeader();
+  });
 
 };
